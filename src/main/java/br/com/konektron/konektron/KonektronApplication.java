357e6ec1029fc04a968bd6b7c7bedda4f8a471d0
ivamgf <programador.ivam@gmail.com>
2022-11-11 12:25:44 -0300

    first commit

---
+++ src/main/java/br/com/konektron/konektron/KonektronApplication.java
@@ -0,0 +1,13 @@
+package br.com.konektron.konektron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonektronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonektronApplication.class, args);
+	}
+
+}
